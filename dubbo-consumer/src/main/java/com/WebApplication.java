47e5c70bc12e7669dae8edda9780ec2a8648fc8b
fanbin <510988896@qq.com>
2019-04-21 18:21:30 +0800

    swagger无法显示问题修复

--- dubbo-consumer/src/main/java/com/WebApplication.java
+++ dubbo-consumer/src/main/java/com/WebApplication.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.study.api.service")
+//@ComponentScan({"com.study.api.service","com.study.config"})
+@ComponentScan("com.study")
 @ImportAutoConfiguration(ConsumerConfiguration.class)
 public class WebApplication {
 
